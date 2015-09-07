5f392728cebf7cdfc13f262df943ed87ee28642d
Albert Iskhakov <aliskhakov@gmail.com>
2015-09-07 23:32:41 +0300

    calc fixed

--- redbeaver/calc.py
+++ redbeaver/calc.py
@@ -2,7 +2,10 @@ def calc(fn_name, eq):
     if fn_name in eq.eq_registry:
         params = {}
         for arg in eq.eq_registry[fn_name]['args']:
-            params[arg] = eq.params.get(arg, calc(arg, eq))
+            if arg in eq.params:
+                params[arg] = eq.params.get(arg)
+            else:
+                params[arg] = calc(arg, eq)
 
         res = eq.eq_registry[fn_name]['fn'](**params)
     else:
--- redbeaver/test.py
+++ redbeaver/test.py
@@ -1,5 +1,6 @@
 import unittest
 from eq import Eq as eq
+from calc import calc
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,6 +13,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(eq.id[1], 'add')
         self.assertEqual(eq.eq_registry['add']['fn'](1, 5), 6)
         self.assertEqual(add(1, 6), 7)
+
+    def test_calc(self):
+        @eq(1)
+        def add(a, b):
+            return a + b
+
+        @eq(2)
+        def a(b):
+            return b * 2
+
+        eq.params['b'] = 4
+
+        calc('add', eq)
+
+        self.assertEqual(eq.params['add'], 12)
 
 
 if __name__ == '__main__':
