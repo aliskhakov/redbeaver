238a93b796f214ff06ed499a72d847e1c5f3aad5
Albert Iskhakov <aliskhakov@gmail.com>
2015-09-07 22:02:47 +0300

    name fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='readbeaver',
+    name='redbeaver',
     version='0.1.1',
     packages=[''],
     url='https://github.com/aliskhakov/redbeaver',
